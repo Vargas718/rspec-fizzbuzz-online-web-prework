9bf443fd3a730cf6cdd188d93db863fb398fe288
Rafael Vargas <vargas718@gmail.com>
2019-01-08 07:21:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,13 @@
 require 'pry'
 
 
-def fizzbuzz(int)
+def fizzbuzz(int, 15)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
     elsif int % 5 == 0 
     "Buzz"
     elsif int % 4 == 0 
+  
   end
+  
 end
